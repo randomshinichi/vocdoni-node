a47e3228a6478c72f1d8700257659347b54cb9fd
Andrew Chiw <randomshinichi4869@gmail.com>
2022-04-14 01:03:25 +0200

    Vote proof uses Arbo_BLAKE2B

--- benchmark/vochain_benchmark_test.go
+++ benchmark/vochain_benchmark_test.go
@@ -232,6 +232,7 @@ func voteBench(b *testing.B, cl *client.Client, s *ethereum.SignKeys,
 	req.CensusID = fmt.Sprintf("%x", censusRoot)
 	req.CensusKey = s.PublicKey()
 	req.Digested = false
+	req.CensusType = models.Census_ARBO_BLAKE2B
 	resp := doRequest("genProof", nil)
 	if len(resp.Siblings) == 0 {
 		b.Fatalf("proof not generated while it should be generated correctly: %v", resp.Message)
@@ -251,8 +252,9 @@ func voteBench(b *testing.B, cl *client.Client, s *ethereum.SignKeys,
 		Nonce:     util.RandomBytes(32),
 		ProcessId: processID,
 		Proof: &models.Proof{
-			Payload: &models.Proof_Graviton{
-				Graviton: &models.ProofGraviton{
+			Payload: &models.Proof_Arbo{
+				Arbo: &models.ProofArbo{
+					Type:     models.ProofArbo_BLAKE2B,
 					Siblings: resp.Siblings,
 				},
 			},
